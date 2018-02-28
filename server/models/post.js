1adb11ef95dac38aee9bb8bd8e184b77c2cdb487
Michael Gacek <gacek.michael@outlook.com>
2018-02-28 16:38:15 +0100

    Update post.js

--- server/models/post.js
+++ server/models/post.js
@@ -6,7 +6,7 @@ const postSchema = new Schema({
   title: { type: 'String', required: true },
   content: { type: 'String', required: true },
   slug: { type: 'String', required: true },
-  votes: { type: 'Number', required: true },
+  votes: { type: 'Number', default: 0 },
   cuid: { type: 'String', required: true },
   dateAdded: { type: 'Date', default: Date.now, required: true },
 });
